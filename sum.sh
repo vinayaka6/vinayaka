facbf3de1d9395fa88775d23f2f0d1593e89ed38
vinayaka <vinayakavinnu07@gmail.com>
2023-05-09 09:14:43 +0000

    adding file addsum.sh

---
+++ sum.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo enter the first number 
+read num1
+
+echo enter the second number 
+read num2
+
+sum=0
+sum=$((num1 + num2))
+
+
+echo sum of two numbers is $num1 and $num2 is $sum
