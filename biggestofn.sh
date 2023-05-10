b8a549f9036ffe84f384f63014d2f8ee001d921b
vinayaka <vinayakavinnu07@gmail.com>
2023-05-10 11:03:15 +0000

    biggest of n numbers

--- biggestofn.sh
+++ biggestofn.sh
@@ -7,10 +7,21 @@ for i in $*
 do
 	if [ $i -gt $BIG ];
 	then
-		BIG=$i
-	fi
-	
+
+BIG=1
+
+for i in $*
+do
+        if [ $i -gt $BIG ];
+        then
+                BIG=$i
+        fi
+
 done
 
+echo "biggest of $* is $BIG"
 
+
+	fi	
+done
 echo "biggest of $* is $BIG"
