6808df4d413b859287678e86c60b2fdd14020492
vinayaka <vinayakavinnu07@gmail.com>
2023-05-10 11:18:51 +0000

    introduction ad sum
    pyramid sh
    
    pyramid h
    
    mul
    
    checkfile
    
    mul.sh
    
    abc and files

--- checkfile.sh
+++ checkfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+
 
 
 echo " enter the name to check"
--- mul.sh
+++ mul.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-
 echo "ENTER THE TWO NUMBERS"
 read num1 num2 
-
-
 SUM=$(($num1 + $num2))
 product=$(($num1 * $num2))
+
+
+
+
 
 
 echo " sum of $num1 and $num2 is $SUM"
