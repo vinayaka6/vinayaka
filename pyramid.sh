6d1d486ce0caaa9b4d791a65a66d2fc7062856ee
vinayaka <vinayakavinnu07@gmail.com>
2023-05-12 17:52:10 +0000

    pyramid

--- pyramid.sh
+++ pyramid.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 read -p "Enter the numbers of rows:" rows
 for (( i=1; i<=rows; i++ ))
 do
@@ -9,6 +8,7 @@ do
 	done
 	for (( k=1; k<=2*i-1; k++))
 	do
+
 		echo -ne "1"
 	done
 	echo " "
